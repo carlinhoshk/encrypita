a793ee6850ac3259cf7d74ae5bf18d1b9d2cc090
carlinhoshk <carlosmdohk@gmail.com>
2023-05-09 23:08:05 -0300

    primeiro commit

---
+++ src/main/java/io/github/carlinhoshk/encrypitademo/model/UsuarioModel.java
@@ -0,0 +1,20 @@
+package io.github.carlinhoshk.encrypitademo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="Usuario")
+public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+}
